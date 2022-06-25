b61afb3d37eb95841b754e64b2732b6d630c5de1
Vahid Moradi <zvm4zvm@gmail.com>
2022-06-25 20:08:55 +0430

    width and height of canvas changed.

--- src/canvas.js
+++ src/canvas.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.height = 800
-canvas.width = 800
+canvas.height = 700
+canvas.width = 700
 
 c.fillStyle = '#ddd'
 c.fillRect(0, 0, canvas.width, canvas.height)
